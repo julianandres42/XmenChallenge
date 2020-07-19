654f5b058eb6bca47ce320b0a7a40a305c33633e
julianandres42 <julianandres42@gmail.com>
2020-07-18 23:38:11 -0500

    adding test to stats

--- app/app_test.go
+++ app/app_test.go
@@ -1,7 +1,9 @@
 package app
 
 import (
+	"XmenChallenge/persistence"
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -26,8 +28,24 @@ func (T2) IsMutant() bool {
 
 func (T2) SetDna(dna []string) {}
 
+type T3 struct {
+}
+
+func (T3) SaveCandidate(db *sql.DB, candidate *persistence.Candidate) error {
+	return nil
+}
+
+func (T3) GetStats(db *sql.DB) (*persistence.Stats, error) {
+	return persistence.GetNewStats(1, 1), nil
+}
+
+func TestInitialize(t *testing.T) {
+	app := App{Evaluator: T{}, DbApi: T3{}}
+	app.Initialize("", "", "", "")
+}
+
 func TestIsMutant(t *testing.T) {
-	app := App{Evaluator: T{}}
+	app := App{Evaluator: T{}, DbApi: T3{}}
 	requestBody, err := json.Marshal(map[string][]string{"dna": {"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}})
 
 	if err != nil {
@@ -46,7 +64,7 @@ func TestIsMutant(t *testing.T) {
 }
 
 func TestIsNotMutant(t *testing.T) {
-	app := App{Evaluator: T2{}}
+	app := App{Evaluator: T2{}, DbApi: T3{}}
 	requestBody, err := json.Marshal(map[string][]string{"dna": {"ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"}})
 
 	if err != nil {
@@ -79,5 +97,24 @@ func TestBadRequest(t *testing.T) {
 	res := rec.Result()
 	if res.StatusCode != http.StatusBadRequest {
 		t.Errorf("expected %d, got: %d", http.StatusOK, res.StatusCode)
+	}
+}
+
+func TestStats(t *testing.T) {
+	app := App{Evaluator: T2{}, DbApi: T3{}}
+	stats := Stats{}
+	requestBody, err := json.Marshal(map[string][]string{"dna": {}})
+	if err != nil {
+		t.Fatalf("could not created request: %v", err)
+	}
+	req, err := http.NewRequest("GET", "/stats", bytes.NewBuffer(requestBody))
+	if err != nil {
+		t.Fatalf("could not created request: %v", err)
+	}
+	rec := httptest.NewRecorder()
+	app.stats(rec, req)
+	json.Unmarshal(rec.Body.Bytes(), &stats)
+	if stats.CountHuman != 1 || stats.CountMutant != 1 || stats.Ratio != 1 {
+		t.Errorf("expected %d, got: %d , %d,  got %d, %d, got %f", 1, stats.CountMutant, 1, stats.CountHuman, 1, stats.Ratio)
 	}
 }
